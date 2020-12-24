101f1c9b23c33ce0df81b447c07967470bfd1323
zhuje <59589720+zhuje@users.noreply.github.com>
2020-12-24 18:36:29 -0500

    RESUME link pdf version

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,8 +4,11 @@ import '../style/style.navbar.css'
 export default class Resume extends React.Component {
     render() {
         return (
-            <div >
+            <div className="container-fluid">
                 <h1> Resume </h1>
+                <a href={"https://drive.google.com/file/d/1W2GFuJBmr3chIpeBqh_Nq-C05gYe9ufN/view?usp=sharing"} className="float-right">
+                    PDF Version
+                </a>
             </div>
 
         )
